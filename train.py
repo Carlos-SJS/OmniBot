c503105ad906700b5e07e9413992522b3dbd14e4
Carlos-SJS <carlos.sjs@outlook.com>
2025-04-01 14:44:16 -0600

    Increased location batch size

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from torch import nn
 import torch.nn.functional as F
 from tqdm import tqdm
 
-def loss_function(logits):
+def loss_function(logits, _ = None):
     labels = torch.arange(logits.shape[0], device=logits.device)
     
     return F.cross_entropy(logits, labels)
@@ -24,13 +24,19 @@ def distance_based_loss(logits, locs):
     lat1, lat2 = lat, lat.T
     lng1, lng2 = lng, lng.T
 
-    dst_matrix = haversine_dist(lat1, lng1, lat2, lng2)
+    dst_matrix = haversine_dist(lat1, lng1, lat2, lng2)/1000
+    dst_matrix = dst_matrix**2
     softmax_logits = logits.softmax(dim=-1)
 
-    return torch.mean(dst_matrix * softmax_logits)/1000
+    return torch.mean(softmax_logits @ dst_matrix)
+
+def combined_loss(logits, locs):
+    cross_entropy = loss_function(logits)
+    dist_loss = distance_based_loss(logits, locs)/2
+    return (cross_entropy + dist_loss)/2
 
 
-def train(train_dataloader, model, optimizer, epoch, device, temperature = 0.07):
+def train(train_dataloader, model, optimizer, epoch, device, temperature = 0.07, loss_func = loss_function):
     print("Starting Epoch", epoch)
 
     bar = tqdm(enumerate(train_dataloader), total=len(train_dataloader))
@@ -44,17 +50,15 @@ def train(train_dataloader, model, optimizer, epoch, device, temperature = 0.07)
         imgs = imgs.to(device)
         locs = locs.to(device)
 
-        #indices = torch.randint(0, model.loc_galery.size(0), (1000,))
-        #rnd_locs = model.loc_galery[indices].to(device)
-        #locs_all = torch.cat([locs, rnd_locs]).to(device)
+        indices = torch.randint(0, model.loc_gallery.size(0), (4000,))
+        rnd_locs = model.loc_gallery[indices].to(device)
+        locs_all = torch.cat([locs, rnd_locs]).to(device)
 
-        img_embeddings, loc_embeddings = model(imgs, locs)
-        img_embeddings = F.normalize(img_embeddings, dim=1)
-        loc_embeddings = F.normalize(loc_embeddings, dim=1)
+        img_embeddings, loc_embeddings = model(imgs, locs_all)
 
-        logits = img_embeddings @ loc_embeddings.T
+        logits = (img_embeddings @ loc_embeddings.T)/temperature
         
-        loss = distance_based_loss(logits, locs)
+        loss = loss_func(logits, locs_all)
 
         loss.backward()
         optimizer.step()
