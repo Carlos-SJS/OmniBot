1ce054ead2a56951565e2d8dfe18ba4026c9737a
Carlos-SJS <carlos.sjs@outlook.com>
2025-04-06 12:18:32 -0600

    Fixed data loading bottlenecks, improved distance based loss based on PIGEON paper

--- dataloader.py
+++ dataloader.py
@@ -21,7 +21,8 @@ class OmniWorldLoader(Dataset):
     def create_preprocessor_train(self):
         self.img_preprocessor = T.Compose([
             T.RandomResizedCrop(self.image_size),
-            T.RandomApply([T.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1)], p=0.5),
+            T.RandomHorizontalFlip(),
+            T.RandomApply([T.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1)], p=0.8),
             T.ToTensor(), 
             T.ConvertImageDtype(torch.float),
             T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
@@ -38,8 +39,9 @@ class OmniWorldLoader(Dataset):
     def load_dataset(self, image_directory):
         coordinates = []
         img_paths = []
+
         for i, data in self.loc_data.iterrows():
-            coordinates.append((data["lat"], data["lng"]))
+            coordinates.append(torch.tensor((data["lat"], data["lng"]), dtype=torch.float))
             img_paths.append(os.path.join(image_directory, f"{i}.jpg"))
         
         return img_paths, coordinates
@@ -48,7 +50,7 @@ class OmniWorldLoader(Dataset):
         return self.loc_data.shape[0]
 
     def __getitem__(self, idx):
-        loc = torch.tensor(self.coordinates[idx], dtype=torch.float)
+        loc = self.coordinates[idx]
         img = im.open(self.imgage_paths[idx]).convert('RGB').crop((20, 16, 620, 616))
         img = self.img_preprocessor(img)
 
--- train.py
+++ train.py
@@ -19,16 +19,23 @@ def haversine_dist(lat1, lng1, lat2, lng2):
     a = torch.sin(dlat/2)**2 + torch.cos(lat1)*torch.cos(lat2)*torch.sin(dlng/2)**2
     return 2*R*torch.arcsin(torch.sqrt(a))
 
-def distance_based_loss(logits, locs):
+def distance_matrix(locs):
     lat, lng = locs[:,0:1], locs[:,1:2]
     lat1, lat2 = lat, lat.T
     lng1, lng2 = lng, lng.T
 
-    dst_matrix = haversine_dist(lat1, lng1, lat2, lng2)/1000
+    return haversine_dist(lat1, lng1, lat2, lng2)
     dst_matrix = dst_matrix**2
     softmax_logits = logits.softmax(dim=-1)
 
     return torch.mean(softmax_logits @ dst_matrix)
+
+def distance_based_loss(logits, locs, temperature = 75):
+    dst_matrix = torch.exp(- distance_matrix(locs)/temperature)
+    softmax_logits = logits.softmax(dim=-1)
+
+    loss = -torch.mean(torch.log(softmax_logits + 1e-8) * dst_matrix[:softmax_logits.shape[0],:])
+    return loss
 
 def combined_loss(logits, locs):
     cross_entropy = loss_function(logits)
@@ -50,11 +57,11 @@ def train(train_dataloader, model, optimizer, epoch, device, temperature = 0.07,
         imgs = imgs.to(device)
         locs = locs.to(device)
 
-        indices = torch.randint(0, model.loc_gallery.size(0), (4000,))
-        rnd_locs = model.loc_gallery[indices].to(device)
-        locs_all = torch.cat([locs, rnd_locs]).to(device)
+        indices = torch.randint(0, model.loc_gallery.size(0), (4000,), device=device)
+        rnd_locs = model.loc_gallery[indices]
+        locs_all = torch.cat([locs, rnd_locs])
 
-        img_embeddings, loc_embeddings = model(imgs, locs_all)
+        img_embeddings, loc_embeddings = model.forward(imgs, locs_all)
 
         logits = (img_embeddings @ loc_embeddings.T)/temperature
         
