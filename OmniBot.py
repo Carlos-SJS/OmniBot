c44091c381029c8f4d6b719a22aadddf2f72bbe1
Carlos-SJS <carlos.sjs@outlook.com>
2025-04-06 12:17:27 -0600

    Added open cilp vitb16 image encoder backbone

--- OmniBot.py
+++ OmniBot.py
@@ -10,12 +10,11 @@ import numpy as np
 import pandas as pd
 from PIL import Image as im
 
-
 class OmniBot(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.location_encoder = LocationEncoder(from_pretrained=False)
+        self.location_encoder = LocationEncoder(from_pretrained=True)
         self.image_encoder = image_encoder.ImageEncoder()
 
         loc_data = pd.read_csv(r"C:\Users\carlo\Documents\Python\Geo\ds\dataset_generation\omniworld_locations.csv")
@@ -31,10 +30,14 @@ class OmniBot(nn.Module):
             T.ConvertImageDtype(torch.float),
             T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ])
+
+        #for param in self.location_encoder.parameters():
+        #    param.requires_grad = False
     
     def to(self, device):
         self.image_encoder.to(device)
         self.location_encoder.to(device)
+        self.loc_gallery = self.loc_gallery.to(device)
         return super().to(device)
 
     def forward(self, img, loc):
--- image_encoder.py
+++ image_encoder.py
@@ -4,6 +4,8 @@ from torchvision.models import vit_b_16, ViT_B_16_Weights
 from torchvision import transforms as T
 
 import timm
+from transformers import CLIPModel, AutoProcessor
+import open_clip
 
 class ImageEncoder(nn.Module):
     def __init__(self, embedding_dim=512):
@@ -16,16 +18,24 @@ class ImageEncoder(nn.Module):
         # self.backbone.reset_classifier(0)
         # feature_dim = self.backbone.num_features
 
-        self.backbone = vit_b_16(weights=ViT_B_16_Weights.DEFAULT)
-        feature_dim = self.backbone.heads.head.in_features
-        self.backbone.heads = nn.Identity()
+        self.backbone, _, _ = open_clip.create_model_and_transforms('ViT-B-16', pretrained='datacomp_l_s1b_b8k')
+        feature_dim = 512
+
+        # self.backbone = vit_b_16(weights=ViT_B_16_Weights.DEFAULT)
+        # feature_dim = self.backbone.heads.head.in_features
+        # self.backbone.heads = nn.Identity()
         
+        #self.backbone = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
+        #feature_dim = 768
+
         for param in self.backbone.parameters():
             param.requires_grad = False
 
         self.mlp = nn.Sequential(
             nn.Linear(feature_dim, 1024),
             nn.ReLU(),
+            #nn.Linear(1024, 512),
+            #nn.ReLU(),
             nn.Linear(1024, embedding_dim)
         )
     
@@ -35,7 +45,9 @@ class ImageEncoder(nn.Module):
         return super().to(device)
 
     def forward(self, x):
-        x = self.backbone(x)
+        # x = self.backbone(x)
+        # x = self.backbone.get_image_features(pixel_values=x)
+        x = self.backbone.encode_image(x)
         x = self.mlp(x)
 
         return x
